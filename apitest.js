3f6e86100f70e4ce74f454437985aa0f8a6292aa
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2021-10-14 19:12:50 -0500

    Srry Did Not Read The Forum Fully

--- apitest.js
+++ apitest.js
@@ -1,4 +1,20 @@
-        window.addEventListener("gamepadconnected", function(e) {
+var gamepads = {};
+
+function gamepadHandler(event, connecting) {
+  var gamepad = event.gamepad;
+  // Note:
+  // gamepad === navigator.getGamepads()[gamepad.index]
+
+  if (connecting) {
+    gamepads[gamepad.index] = gamepad;
+  } else {
+    delete gamepads[gamepad.index];
+  }
+}
+
+window.addEventListener("gamepadconnected", function(e) { gamepadHandler(e, true); }, false);
+window.addEventListener("gamepaddisconnected", function(e) { gamepadHandler(e, false); }, false);  
+window.addEventListener("gamepadconnected", function(e) {
   alert("Gamepad connected at index %d: %s. %d buttons, %d axes.",
     e.gamepad.index, e.gamepad.id,
     e.gamepad.buttons.length, e.gamepad.axes.length);
